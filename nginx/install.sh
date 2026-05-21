de609ae9ff5e74987b1f3d95b503da042acd6726
juniorFandom <juniorfandom95@gmail.com>
2026-05-21 14:24:55 +0100

    fix: use debian:12-slim to build images

---
+++ nginx/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "=========================================="
+echo "🔄 Mise à jour du système..."
+echo "=========================================="
+
+sudo apt update -y && sudo apt upgrade -y
+
+
+echo "=========================================="
+echo "📦 Installation de NGINX..."
+echo "=========================================="
+
+sudo apt install nginx -y
+
+echo "=========================================="
+echo "🚀 Configuration du service NGINX..."
+echo "=========================================="
+
+sudo cp nginx.conf /etc/nginx/nginx.conf
+
+echo "=========================================="
+echo "🚀 Démarrage du service NGINX..."
+echo "=========================================="
+
+sudo systemctl start nginx
+
+echo "=========================================="
+echo "🔁 Activation au démarrage..."
+echo "=========================================="
+
+sudo systemctl enable nginx
+
+
+echo "=========================================="
+echo "✅ Vérification du statut NGINX..."
+echo "=========================================="
+
+sudo systemctl status nginx
